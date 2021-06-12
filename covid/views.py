b927243465f4ac3efad6e1fb0b8f34a2c7cea4f1
Syed Ali <syedali3267@gmail.com>
2021-06-12 18:10:18 +0100

    Added Dictionary/context

--- covid/views.py
+++ covid/views.py
@@ -5,12 +5,18 @@ import requests
 def home(request):
     url = "https://api.covid19api.com/summary"
 
-#    payload = {}
-#    headers = {}
+    country = 'Albania'
 
-    response = requests.request.get( url).json()
-#                                     , headers=headers, data=payload)
+    response = requests.get(url.format(country)).json()
 
-    print(response.text)
+#    print(response)
 
-    return render(request, 'home.html')
+    stats = {
+        'country': country,
+        'total': response['Global']['NewConfirmed'],
+        'new': response['Global'][0]['NewDeaths'],
+    }
+
+    context = {'stats': stats}
+
+    return render(request, 'home.html', context)
