8b7765349538038528ea8df68a40d0ac225422b5
Syed Ali <syedali3267@gmail.com>
2021-09-01 15:34:31 +0100

    Added API

--- stockapp/views.py
+++ stockapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .forms import StockForm
+import requests
+
 
 
 def home(request):
+    url = "https://api.tiingo.com/tiingo/daily/<ticker>/prices"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': '14dfe59b364eb4d7ad9a62a03bfda423b3031073'
+    }
+    requestresponse = requests.get(url, headers=headers)
+
     form = StockForm
     context = {'form': form}
     return render(request, 'home.html', context)
