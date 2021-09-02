eb198faabcf72d759ed464ba7ac193745151ebb7
Syed Ali <syedali3267@gmail.com>
2021-09-02 16:05:05 +0100

    Created for loop

--- stockapp/views.py
+++ stockapp/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from .forms import StockForm
 import requests
+from .models import StockList
 
+headers = {
+        'Content-Type': 'application/json',
+        'Authorization' : '14dfe59b364eb4d7ad9a62a03bfda423b3031073'
+}
 
 
 def home(request):
-    url = "https://api.tiingo.com/tiingo/daily/<ticker>/prices"
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': '14dfe59b364eb4d7ad9a62a03bfda423b3031073'
-    }
-    requestresponse = requests.get(url, headers=headers)
+   url = "https://api.tiingo.com/tiingo/daily/{}/prices"
+   response = requests.get(url,headers=headers)
 
+
+
+    name = StockList.objects.all()
     form = StockForm
-    context = {'form': form}
+    context = {'form': form, 'name': name}
     return render(request, 'home.html', context)
