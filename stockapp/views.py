8a0ac4f01f19ba81a9e6783f6a12cad6d33b5919
Syed Ali <syedali3267@gmail.com>
2021-09-05 11:36:27 +0100

    Added space to align with PEP8 rules

--- stockapp/views.py
+++ stockapp/views.py
@@ -3,16 +3,14 @@ from .forms import StockForm
 import requests
 from .models import StockList
 
-
 headers = {
     'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
     'x-rapidapi-key': "3be7f48f16msh9bcc8b5167481bdp110da2jsn49023f5afc93"
-    }
+}
 
 
 def home(request):
-
-     url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-profile"
+    url = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-profile'
 
      querystring = {"symbol": "AAPL", "region": "US"}
      response = requests.request("GET", url, headers=headers, params=querystring)
