b4c5cf0e453e03326f3d942ef39fbfef544e2231
Syed Ali <syedali3267@gmail.com>
2021-09-13 15:41:25 +0100

    Queried database

--- stockapp/views.py
+++ stockapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import StockForm
 import requests
 
+from .models import StockList
 
 headers = {
     'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
@@ -11,6 +12,11 @@ headers = {
 
 def home(request):
     url = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-profile'
+    querystring = {'symbol': 'TSLA'}
+    # querying database
+    allstocks = StockList.objects.all()
+
+    stock_data =[]
 
     querystring = {'symbol': 'AAPL'}
     response = requests.get(url, headers=headers, params=querystring).json()
@@ -22,6 +28,7 @@ def home(request):
     }
 
     form = StockForm
+
     # context has form and dictionary
     context = {'form': form, 'stock_data': stock_data}
     return render(request, 'home.html', context)
